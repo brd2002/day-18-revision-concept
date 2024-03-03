82727cf5b48bfa6058390c2510d7fcab7032f112
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-03 19:42:32 +0530

    completed dao

---
+++ app/src/main/java/com/example/revisionconcept/rommdb/dao/ContactDao.kt
@@ -0,0 +1,25 @@
+package com.example.revisionconcept.rommdb.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.revisionconcept.rommdb.entity.Contact
+
+@Dao
+interface ContactDao {
+    // insert the contact
+    @Insert(onConflict = OnConflictStrategy.IGNORE) // if conflict happen then it will happen
+    fun createContact (contact: Contact)
+    // update the contact
+    @Update
+    fun updataContact (contact: Contact)
+    // return the contact list
+    @Query("SELECT * FROM CONTACT")
+    fun readContact() : List<Contact>
+    // delete the contact
+    @Delete
+    fun deleteContact(contact: Contact)
+}
--- app/src/main/java/com/example/revisionconcept/rommdb/entity/Contact.kt
+++ app/src/main/java/com/example/revisionconcept/rommdb/entity/Contact.kt
@@ -1,12 +1,14 @@
 package com.example.revisionconcept.rommdb.entity
 
+import android.provider.ContactsContract.RawContacts.Data
 import androidx.room.Entity
 
 @Entity
 class Contact (
-    var id : Int ,
-    var profile : Int ,
+    var id : Int ? ,
+    var profile : Int ? ,
     var name : String ,
     var phonenumber : String ,
-    var emailid : String
+    var emailid : String ? ,
+//    var data  : Data
 )
